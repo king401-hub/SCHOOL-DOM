20cd68d0826de5232e924174f2affb408b29fba2
risenmen-job <harleyharp123@gmail.com>
2026-02-10 16:14:24 +0100

    Work on offline exams module

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-class TimeStampedModel(models.Model):
+class TenantAwareModel(models.Model):
+    tenant = models.ForeignKey('tenants.Tenant', on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
         abstract = True
+
